560d86a2c6f7e66d229a0f25871ecbdbac3269e9
vnaxel <axel.yvenou@gmail.com>
2022-08-29 15:14:21 +0200

    remove pom.xml

--- src/main/java/com/hello/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/hello/helloWorld/HelloWorldApplication.java
@@ -1,9 +1,5 @@
 package com.hello.helloWorld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
