34791e163d4fe031a9c1979bd081750b66adea65
Axel <81324455+vnaxel@users.noreply.github.com>
2022-08-29 15:29:56 +0200

    Update HelloWorldApplication.java

--- src/main/java/com/hello/helloWorld/HelloWorldApplication.java
+++ src/main/java/com/hello/helloWorld/HelloWorldApplication.java
@@ -3,7 +3,7 @@ package com.hello.helloWorld;
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello update!");
 	}
 
 }
